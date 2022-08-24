9d9c7ad3c9ddc979dee3db4cfba678aea5e0e538
Clara-Lea <50104903+claralea@users.noreply.github.com>
2022-08-24 17:49:40 -0400

    Save simulated data so that it can be imported by the used and ran in example

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Simulated data
+#'
+#' A dataset used to replicate results from the publication. We assume correct outcome and correct imputation
+#' Generated using the function
+#' simulation_data <- DataGeneration(n_lab = 400, n_unlab = 20000, p = 10, rho = 0.4 signal = c(1, 1, 0.5, 0.5), model_specification = 'outcome_correct_imputation_correct',num_strata = 2)
+#' \itemize{
+#'   \item covariates_lab. labeled set X
+#'   \item covariates_unlab. unlabeled set X
+#'   \item S_lab. labeled S
+#'   \item S_unlab. unlabeled S
+#'   \item Y_lab. outcome 
+#'   \item samp_prob. probabilities
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name simulation_data
+#' @usage data(simulation_data)
+#' @format A list of parameters and vectors
+NULL
